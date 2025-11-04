9c89f8e09484d43aa77a98fd683bfa6ac87db707
Vlada Stankovic <svlada@gmail.com>
2025-11-04 23:28:26 +0100

    d

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("MMMM dd, yyyy");
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy-MM-dd");
   });
 
   eleventyConfig.addFilter("dateYear", dateObj => {
